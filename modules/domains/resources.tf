baeeee655ac596b1b243f45039985bad91e85bb4
Alejandro Acho <61828943+Alejandroacho@users.noreply.github.com>
2023-01-06 23:42:27 +0100

    refactor: Domain resources

--- modules/domains/resources.tf
+++ modules/domains/resources.tf
@@ -14,39 +14,39 @@ resource "digitalocean_domain" "production_domain" {
 
 resource "digitalocean_record" "production_record" {
   domain = digitalocean_domain.production_domain.id
-  type   = "A"
-  name   = "*."
   value  = var.PRODUCTION_IP_ADDRESS
+  type   = "A"
+  name   = "*"
 }
 
 resource "digitalocean_domain" "dev_domain" {
   # only create if CREATE_NON_PRODUCTION_RESOURCES is true
-  count  = var.CREATE_NON_PRODUCTION_RESOURCES ? 1 : 0
   ip_address = var.DEV_STAGING_IP_ADDRESS
+  count      = var.CREATE_NON_PRODUCTION_RESOURCES ? 1 : 0
   name       = "dev.${var.DOMAIN}"
 }
 
 resource "digitalocean_record" "dev_record" {
   # only create if CREATE_NON_PRODUCTION_RESOURCES is true
-  count  = var.CREATE_NON_PRODUCTION_RESOURCES ? 1 : 0
   domain = digitalocean_domain.dev_domain[0].id
-  type   = "A"
-  name   = "*."
+  count  = var.CREATE_NON_PRODUCTION_RESOURCES ? 1 : 0
   value  = var.DEV_STAGING_IP_ADDRESS
+  type   = "A"
+  name   = "*"
 }
 
 resource "digitalocean_domain" "staging_domain" {
   # only create if CREATE_NON_PRODUCTION_RESOURCES is true
-  count  = var.CREATE_NON_PRODUCTION_RESOURCES ? 1 : 0
   ip_address = var.DEV_STAGING_IP_ADDRESS
+  count      = var.CREATE_NON_PRODUCTION_RESOURCES ? 1 : 0
   name       = "staging.${var.DOMAIN}"
 }
 
 resource "digitalocean_record" "staging_record" {
   # only create if CREATE_NON_PRODUCTION_RESOURCES is true
-  count  = var.CREATE_NON_PRODUCTION_RESOURCES ? 1 : 0
   domain = digitalocean_domain.staging_domain[0].id
-  type   = "A"
-  name   = "*."
+  count  = var.CREATE_NON_PRODUCTION_RESOURCES ? 1 : 0
   value  = var.DEV_STAGING_IP_ADDRESS
+  type   = "A"
+  name   = "*"
 }
